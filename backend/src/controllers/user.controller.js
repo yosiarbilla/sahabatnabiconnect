4193125fbd6d886000fdd6d1db4d217d9b462fcd
yosiarbilla <yosiarbl16@gmail.com>
2025-07-12 10:38:51 +0700

    fix user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/user.js";
 import FriendRequest from "../models/FriendRequest.js";
 
 export async function getRecommendedUsers(req, res) {
